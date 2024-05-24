4b33e1b5fc4fb71ea7339dd36fb04ffa1be92808
cobi <111162430+jhfuentebella@users.noreply.github.com>
2024-05-24 20:19:29 +0800

    Update 02_Results.py
    add columns

--- pages/02_Results.py
+++ pages/02_Results.py
@@ -3,4 +3,9 @@ import streamlit as st
 st.title("Results")
 st.write("Here you can put each of your key results.")
 
-st.columns()
+c1,c2 = st.columns((0.25,1))
+
+with c<1:
+  st.write("hello")
+with c<2:
+  st.write("bye")
