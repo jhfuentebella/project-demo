348a6ea8142b26f6c79de2bfef2f2db144bbe2df
cobi <111162430+jhfuentebella@users.noreply.github.com>
2024-05-24 20:05:07 +0800

    Update test.py
    added on call plot

--- test.py
+++ test.py
@@ -3,3 +3,21 @@ st.write("Hello, let's learn how to build a streamlit app together")
 st.header("hello")
 st.markdown("--")
 st.subheader("hello again")
+
+
+import pandas as pd
+
+df = pd.read_csv('cc_rfm.csv')
+st.dataframe(df) 
+
+# Display plot (on-call)
+
+# add these lines in requirements.in
+# matplotlib
+# numpy
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+rand=np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()ax.hist(rand, bins=15)st.pyplot(fig)
